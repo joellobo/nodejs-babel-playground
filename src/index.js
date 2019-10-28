ddf046a5645d79aeb60e655f166e09b928694f14
Joel Lobo <joellobo@gmail.com>
2019-10-28 10:41:16 -0300

    adding cors and env variable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import 'dotenv/config';
-console.log('Hello Node.js project.');
-console.log(process.env.MY_SECRET);
+import cors from 'cors';
+import express from 'express';
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
