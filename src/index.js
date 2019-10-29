a633fe13a0bcb893d931d10acb6ce72040c93c80
Joel Lobo <joellobo@gmail.com>
2019-10-29 14:44:37 -0300

    adding routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import 'dotenv/config';
-import cors from 'cors';
+...
 import express from 'express';
 const app = express();
-app.use(cors());
+...
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  return res.send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
 });
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
